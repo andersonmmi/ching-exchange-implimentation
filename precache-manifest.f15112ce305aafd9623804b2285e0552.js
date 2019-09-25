1de181c6acf958ef881c7b48f7b9c69c895d6c2c
Aaron Anderson <aaronlynanderson@gmail.com>
2019-09-24 19:12:11 -0500

    Updates

---
+++ precache-manifest.f15112ce305aafd9623804b2285e0552.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4229b086309b88e573f0",
+    "url": "/ching-exchange-implimentation/static/js/runtime~main.4229b086.js"
+  },
+  {
+    "revision": "4f3deb7476810876c3bd",
+    "url": "/ching-exchange-implimentation/static/js/main.4f3deb74.chunk.js"
+  },
+  {
+    "revision": "5dda55c1f322ff82c454",
+    "url": "/ching-exchange-implimentation/static/js/1.5dda55c1.chunk.js"
+  },
+  {
+    "revision": "39a127282679375ddd0e1797be9dcf22",
+    "url": "/ching-exchange-implimentation/index.html"
+  }
+];
